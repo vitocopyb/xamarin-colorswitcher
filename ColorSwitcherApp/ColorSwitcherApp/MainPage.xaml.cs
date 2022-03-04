13863589526bf41d92283b330a3c7aab041bc826
Victor Sepulveda <vsepulveda@gmail.com>
2022-03-04 01:55:46 -0300

    Fin app

--- ColorSwitcherApp/ColorSwitcherApp/MainPage.xaml.cs
+++ ColorSwitcherApp/ColorSwitcherApp/MainPage.xaml.cs
@@ -14,5 +14,28 @@ namespace ColorSwitcherApp
         {
             InitializeComponent();
         }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double red = redSlider.Value;
+            double green = greenSlider.Value;
+            double blue = blueSlider.Value;
+
+            Color bgColor = new Color(red, green, blue);
+            boxColor.BackgroundColor = bgColor;
+            //string hex = ColorToHex(bgColor);
+            string hex = bgColor.ToHex();
+            lblDisplay.Text = $"#{hex.Substring(3, 6)}";
+        }
+
+        private string ColorToHex(Color color)
+        {
+            int alpha = (int)(color.A * 255);
+            int red = (int)(color.R * 255);
+            int green = (int)(color.G * 255);
+            int blue = (int)(color.B * 255);
+
+            return $"#{alpha:X2}{red:X2}{green:X2}{blue:X2}";
+        }
     }
 }
